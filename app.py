e7bde566cd801592d98382bc7d560ef0feee58bf
sgwryi <kisthuba@gmail.com>
2025-08-30 13:12:02 +0900

    押忍！

--- app.py
+++ app.py
@@ -1,50 +1,53 @@
-import os
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, send_messages
-from dotenv import load_dotenv
+    import os
+    from flask import Flask, request, abort
+    from linebot import LineBotApi, WebhookHandler
+    from linebot.exceptions import InvalidSignatureError
+    from linebot.models import MessageEvent, TextMessage, TextSendMessage, send_messages
+    from dotenv import load_dotenv
 
-load_dotenv(override=True)
+    load_dotenv(override=True)
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ["ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
-
-
-@app.route("/")
-def index():
-    return "You call index()"
+    line_bot_api = LineBotApi(os.environ["ACCESS_TOKEN"])
+    handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
 
 
-@app.route("/callback", methods=["POST"])
-def callback():
-    """Messaging APIからの呼び出し関数"""
-    # LINEがリクエストの改ざんを防ぐために付与する署名を取得
-    signature = request.headers["X-Line-Signature"]
-    # リクエストの内容をテキストで取得
-    body = request.get_data(as_text=True)
-    # ログに出力
-    app.logger.info("Request body: " + body)
-
-    try:
-        # signature と body を比較することで、リクエストがLINEから送信されたものであることを検証
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        # クライアントからのリクエストに誤りがあったことを示すエラーを返す
-        abort(400)
-
-    return "OK"
+    @app.route("/")
+    def index():
+        return "You call index()"
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    recieved_message = recieved_message[7:-4]
-    send_messages = recieved_message(event.reply_token, TextSendMessage)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    @app.route("/callback", methods=["POST"])
+    def callback():
+        """Messaging APIからの呼び出し関数"""
+        # LINEがリクエストの改ざんを防ぐために付与する署名を取得
+        signature = request.headers["X-Line-Signature"]
+        # リクエストの内容をテキストで取得
+        body = request.get_data(as_text=True)
+        # ログに出力
+        app.logger.info("Request body: " + body)
+
+        try:
+            # signature と body を比較することで、リクエストがLINEから送信されたものであることを検証
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            # クライアントからのリクエストに誤りがあったことを示すエラーを返す
+            abort(400)
+
+        return "OK"
 
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_message(event):
+    #     recieved_message = recieved_message[7:-4]
+    #     send_messages = recieved_message(event.reply_token, TextSendMessage)
+    user_text = event.message.text
+    reply_text = f"{user_text} 押忍！"
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+
+
+    if __name__ == "__main__":
+        port = int(os.getenv("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
